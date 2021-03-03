44556dddaaa6603bfa7c10e25f35a247ad440767
jeford23 <jeford22@gmail.com>
2021-03-03 16:26:58 -0500

    Created Models and Controllers

---
+++ joshuaford-project1.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+using joshuaford_project1.Library;
+using joshuaford_project1.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace joshuaford_project1.WebUI.Models
+{
+    [ApiController]
+    public class OrderContoller : ControllerBase
+    {
+        
+    }
+}
